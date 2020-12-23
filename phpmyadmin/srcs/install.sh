0ff9c84b359f9c921a94c23e08def8800a3cf3f6
Aymen Ghazali <ayghazal@e1r4p11.1337.ma>
2020-12-23 11:26:28 +0100

    phpmyadmin (y)

--- phpmyadmin/srcs/install.sh
+++ phpmyadmin/srcs/install.sh
@@ -22,6 +22,3 @@ wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-l
 tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
-cd phpmyadmin
-mv * ../
-cd .. && rm -rf phpmyadmin
