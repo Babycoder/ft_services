e10e09e43759ddd06342f54d026e1db2185212e5
Aymen Ghazali <ayghazal@e1r11p2.1337.ma>
2021-01-04 19:09:35 +0100

    Loadbalancer

--- mysql/srcs/install.sh
+++ mysql/srcs/install.sh
@@ -3,12 +3,8 @@
 apk update
 apk add bash
 apk add mysql mysql-client
-apk add openrc nginx
+apk add openrc
 rc-status
 touch run/openrc/softlevel
-adduser -D -g 'www' www
-mkdir /www
-chown -R www:www /www
-chown -R www:www /var/lib/nginx
 /etc/init.d/mariadb setup
 bash
